1607ea5575ef25a9ff027f368d87ef4fb0673b0d
fuzebox1 <fuzebox@bahnhof.se>
2020-03-18 20:38:51 +0000

    New features, code restructoring etc.

--- btcticker.py
+++ btcticker.py
@@ -1,66 +1,139 @@
-#! /usr/bin/python3
-
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import argparse
+from datetime import datetime
+from pytz import timezone
+from PIL import Image, ImageFont, ImageDraw
+from inky import InkyPHAT
+from font_fredoka_one import FredokaOne
+from font_hanken_grotesk import HankenGroteskMedium
+from exchanges.bitfinex import Bitfinex
 """
 TODO:
 Add simple logging using loguru? http://github.com/Delgan/loguru
-Place text statically, not by division as below.
 Price history in smaller text under main price.
-Price time.
-Use TinyDB?
-Big price move in Yellow, small White.
-Make "package" for ppl to download and run on their phat.
-Maybe make one for both phat and what. Args for type and colors.
+If btc-api dont support lastprice build a tinydb? also good to have
+a price history locally anyways. Maybe use to chart a graph or smt.
+Big price move in Yellow, small in White. Or use arrow icons.
+- Add args for display type and color. Also currencypair. fallback defaults.
 """
-
-## Dependencies:
-## Install python modules
-# sudo pip3 search/install pillow, inky, fredokaone, bitcoin-price-api, loguru
-from PIL import Image, ImageFont, ImageDraw
-from inky import InkyPHAT
-#from font_fredoka_one import FredokaOne
-from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
-from exchanges.bitfinex import Bitfinex
-
-# Im using a yellow inky pHAT.
-# If you use a wHAT change "InkyPHAT" to "InkyWHAT"
-# And if you have another color, change "yellow" to "red" or "black".
-inky_display = InkyPHAT("yellow")
-
-
+"""
+This program is made to run on the Raspberry Pi (Zero W) with the Pimoroni
+e-ink display InkyPHAT.
+Dependencies:
+Install python modules
+pip search/install inky, pillow, bitcoin-price-api, loguru, pytz
+And a font package, either of the following for example:
+font-hanken-grotesk, font-fredoka-one, font-font-awesome, nerdfonts
+"""
+# https://docs.python.org/2/howto/argparse.html
+parser = argparse.ArgumentParser()
+parser.add_argument("--color", "-c", type=str, required=True, choices=["red", "black", "yellow"], help="InkyPHAT Display Color")
+parser.add_argument("--bordercolor", "-bc", type=str, required=False, choices=["black", "white", "red", "yellow"], help="Set border color.")
+parser.add_argument("--pricecolor", "-pc", type=str, required=False, choices=["black", "white", "red", "yellow"], help="Set color of price.")
+#parser.add_argument("--exchange", "-e")
+#parser.add_argument("--currencypair", "-cp", type=str, required=False, choices=["btcusd", "btceur", "ethusd", "etheur"], help="Currency pair to print. ex. 'btcusd' or 'etheur'.")
+#parser.add_argument("--daemon", "-d", type=str, required=False, help="Run in the background.")
+#parser.add_argument("--updatetimer", "-t", type=int, required=False, choices=["5", "15", "30", "60"], help="How often to update in minutes. 5, 15, 30, 60".)
+#--verbose
+args = parser.parse_args()
+color = args.color
+bordercolor = args.bordercolor
+pricecolor = args.pricecolor
+#updatetimer = args.updatetimer
+# What PHAT color u has?
+inky_display = InkyPHAT(color)
 # Set border color.
 inky_display.set_border(inky_display.YELLOW)
+# Set path to where this file is at.
+PATH = os.path.dirname(__file__)
+# Set time and format (google python datetime module for formatting info)
+# Get time in UTC, and convert it to your timezone, then set localtime.
+# This pretty much prints time over the full screen width
+timeformat = "%H:%M:%S, %a, %b %d, %Y"
+utctime = datetime.now(timezone("UTC"))
+timezone = utctime.astimezone(timezone("Europe/Stockholm"))
+localtime = timezone.strftime(timeformat)
+# Get price. Yes, one line of code, thanks to bitcoin-price-api.
+# Plan to use crypto-price-api (fork) and add more coins later.
+# And also format it to remove decimals.
+btc = Bitfinex().get_current_price()
+price = format(btc, ".0f")
+
+# config - make args and if none defaults
+
+# currencypair and exchange
+pair = "btcusd"
+exchange = "bitfinex"
+
+# Set fonts and font sizes
+pricefont = ImageFont.truetype(FredokaOne, 36)
+timefont = ImageFont.truetype(HankenGroteskMedium, 16)
+msgfont = ImageFont.truetype(HankenGroteskMedium, 16)
+
+# Placement of texts.
+# x, y starts at top left corner. Display is 212x104px.
+# getsize is the size of the text in px, depends on fontsize and amount of text.
+
+wprice, xprice = pricefont.getsize(price)
+pricex = 80
+pricey = 27
+
+wdate, hdate = timefont.getsize(localtime)
+datex = 1
+datey = 84
+
+wpair, hpair = msgfont.getsize(pair)
+pairx = 0                                                                # Top
+pairy = 0                                                                # Left
+
+wex, hex = msgfont.getsize(exchange)
+exx = (inky_display.WIDTH - wex)                                         # Top
+exy = (inky_display.HEIGHT - hex) - (inky_display.HEIGHT - hex)          # Right
+
+#Debugging text size and placements
+print("Price text size: ", wprice, "x", xprice, "px")
+print("Price text placement: ", pricex, "x", pricey, "px")
+print("Timedate text size: ", wdate, "x", hdate, "px")
+print("Timedate text placement: ", datex, "x", datey, "px" )
+print("Currency text size: ", wpair, "x", hpair, "px")
+print("Currency text placement: ", pairx, "x", pairy, "px")
+print("Exchange text size: ", wex, "x", hex, "px")
+print("Exchange text placement: ", exx, "x", exy, "px")
+
+
 
 
 # Set the image. Has to be a specific color palette and size.
 # I used GIMP to edit one of the example images from the inky git.
-# You need the inky-palete.gpl imported and active in GIMP. 
-img = Image.open("/home/pi/code/btc-backdrop-small.png")
-text = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-draw = ImageDraw.Draw(img)
+# You need the inky-palete.gpl imported and active in GIMP.
+
+"""Create a black background mask to write text and images on. I guess if using
+full sized black, white, yellow, red .png image you dont need it? Maybe only for
+placing small images?"""
+# Set background mask as black image - actually draws 8-bit pixels all over.
+bg_mask = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT), (inky_display.BLACK))
+#bgimage = Image.open(os.path.join(PATH, "resources/black-backdrop.png"))
+draw = ImageDraw.Draw(bg_mask)
+# Icons
+#usdicon = Image.open(os.path.join(PATH, "resources/usd-icon.png"))
+#btcicon = Image.open(os.path.join(PATH, "resources/btc-icon.png"))
+#sunicon = Image.open(os.path.join(PATH, "resources/icon-sun.png"))
+#sunicon.paste(bgimage, (28, 36), bg_mask)
 
 
-# Get price. Yes, one line of code, thanks to bitcoin-price-api.
-# And also format it to remove decimals.
-btc = Bitfinex().get_current_price()
-message = format(btc, ".0f")
-
-
-# Set parameterss of text, placement, color, font.
-font = ImageFont.truetype(HankenGroteskBold, 40)
-w, h = font.getsize(message)
-x = (inky_display.WIDTH / 1.3) - (w / 1.3)
-y = (inky_display.HEIGHT / 2.25) - (h / 2.25)
 # My image has black background so im printing text in white.
-# Swap WHITE here for RED, YELLOW or BLACK.
-draw.text((x, y), message, inky_display.WHITE, font)
+# White seems to stick to the screen less. Yellow stays even after several cleaning cycles.
+# Plan to make this an arg later.
+# draw is what to draw on, the image backdrop
 
-
-# Logging. Timestamped and success or error.
-#
-
+draw.text((pricex, pricey), price, inky_display.WHITE, pricefont)
+draw.text((datex, datey), localtime, inky_display.WHITE, timefont)
+draw.text((pairx, pairy), pair.upper(), inky_display.WHITE, msgfont)
+draw.text((exx, exy), exchange.capitalize(), inky_display.WHITE, msgfont)
 
 # Finally print it all
-inky_display.set_image(img)
+#inky_display.set_image(backdrop)
+inky_display.set_image(bg_mask)
 inky_display.show()
-
